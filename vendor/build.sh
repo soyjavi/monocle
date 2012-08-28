81516869541b59101054b53e6ea8344963b2fcef
@soyjavi <javi@tapquo.com>
2012-08-28 19:46:15 +0200

    New Build.sh

--- vendor/build.sh
+++ vendor/build.sh
@@ -12,6 +12,7 @@ clear
 echo -e "\033[0m"============================ MONOCLE COMPILER ============================
 ## Files to compile
 FILES_TO_COMPILE=""
+FILES_TO_JOIN=""
 
 #Main
 DIR=$SOURCES$NAMESPACE
@@ -20,7 +21,12 @@ FILES=(js model.js controller.js view.js mustache.js)
 for file in "${FILES[@]}"
 do
     FILES_TO_COMPILE=$FILES_TO_COMPILE" --js "$DIR$file
+    FILES_TO_JOIN=$FILES_TO_JOIN" "$DIR$file
 done
+
+#UNCOMPRESED
+cat $FILES_TO_JOIN > $BUILDPATH/monocle.debug.js
+echo -e "\033[32m  [BUILD]: monocle.debug.js\033[0m"
 
 #MINIFIED
 java -jar $COMPILER $FILES_TO_COMPILE --js_output_file $BUILDPATH/$BUILDFILE
